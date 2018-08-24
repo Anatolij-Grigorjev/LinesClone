bcfe42e16536eb4579c3c36b6f7a77991709fd85
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-08-24 21:00:27 +0300

    add initial upper points HUD code

--- core/src/com/tiem625/lines/actors/ui/GridHUDBackground.kt
+++ core/src/com/tiem625/lines/actors/ui/GridHUDBackground.kt
@@ -1,30 +1,38 @@
 package com.tiem625.lines.actors.ui
 
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.tiem625.lines.assets.Assets
 
-class GridHUDBackground(x: Float, y: Float, width: Float, height: Float) : Actor() {
+class GridHUDBackground(x: Float, y: Float, width: Float, height: Float, color: Color) : Actor() {
 
     val texture: Texture
 
     init {
-        Assets.manager.finishLoadingAsset(Assets.tile)
-        texture = Assets.manager.get(Assets.tile)
+
+        val pixmap = Pixmap(width.toInt(), height.toInt(), Pixmap.Format.RGB888).apply {
+            setColor(color)
+            fillRectangle(0, 0, this.width, this.height)
+        }
+        texture = Texture(pixmap).apply {
+            pixmap.dispose()
+        }
 
         this@GridHUDBackground.x = x
         this@GridHUDBackground.y = y
         this@GridHUDBackground.width = width
         this@GridHUDBackground.height = height
-        color = Color.YELLOW
+        this@GridHUDBackground.color = color
     }
 
     override fun draw(batch: Batch?, parentAlpha: Float) {
         batch?.let {
-            it.setColor(color.r, color.g, color.b, color.a * parentAlpha)
             it.draw(texture, x, y, width, height)
         }
     }
+
+
 }
--- core/src/com/tiem625/lines/stages/ui/GridHUD.kt
+++ core/src/com/tiem625/lines/stages/ui/GridHUD.kt
@@ -1,20 +1,71 @@
 package com.tiem625.lines.stages.ui
 
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.Group
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.viewport.Viewport
 import com.tiem625.lines.GridGlobals
 import com.tiem625.lines.actors.ui.GridHUDBackground
 
 class GridHUD(viewport: Viewport) : Stage(viewport) {
 
+    val backgroundGroup = Group().apply {
+        x = 0.0f
+        y = viewport.worldHeight - GridGlobals.HUD_HEIGHT
+        width = viewport.worldWidth
+        height = GridGlobals.HUD_HEIGHT
+
+        this@GridHUD.addActor(this)
+    }
 
     val background = GridHUDBackground(
             x = 0.0f,
-            y = viewport.worldHeight - GridGlobals.HUD_HEIGHT,
+            y = 0.0f,
             width = viewport.worldWidth,
-            height = GridGlobals.HUD_HEIGHT
+            height = GridGlobals.HUD_HEIGHT,
+            color = Color.BROWN
     ).apply {
-        this@GridHUD.addActor(this)
+        backgroundGroup.addActor(this)
+    }
+
+    val pointsPadding = 15.0f
+
+    val pointsGroup = Group().apply {
+        x = pointsPadding
+        y = pointsPadding
+        //padded left and right
+        width = viewport.worldWidth - 2 * pointsPadding
+        //padded up and down
+        height = GridGlobals.HUD_HEIGHT - 2 * pointsPadding
+
+        backgroundGroup.addActor(this)
+    }
+
+    val pointsBg = GridHUDBackground(
+            x = 0.0f,
+            y = 0.0f,
+            //padded left and right
+            width = viewport.worldWidth - 2 * pointsPadding,
+            //padded up and down
+            height = GridGlobals.HUD_HEIGHT - 2 * pointsPadding,
+            color = Color.BLACK
+    ).apply {
+        pointsGroup.addActor(this)
+    }
+
+    val pointsLabel = Label("0000", Label.LabelStyle(
+            GridGlobals.pointsLabelFont,
+            Color.WHITE)).apply {
+        setAlignment(Align.right)
+        pointsGroup.addActor(this)
+        setFontScale(
+                parent.width / width / 5, parent.height / height / 3
+        )
+        //center by Y with X in the right align
+        x = parent.width - width - pointsPadding
+        y = parent.height / 2 - height / 2
     }
 
 }
