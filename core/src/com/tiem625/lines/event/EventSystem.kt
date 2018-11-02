ef65ac60fff8b16c93a15e219ac375731af8c085
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2018-11-02 20:21:10 +0200

    retouch event handler naming and architecture

--- core/src/com/tiem625/lines/event/EventSystem.kt
+++ core/src/com/tiem625/lines/event/EventSystem.kt
@@ -5,9 +5,17 @@ import java.util.*
 object EventSystem {
 
     data class GameEventTypeHandler(
-            val type: GameEventTypes,
-            val handler: (GameEvent) -> Unit
-    )
+            val handlerType: GameEventTypes,
+            private val handler: (GameEvent) -> Unit
+    ) {
+        fun handle(event: GameEvent) {
+            if (event.type != handlerType) {
+                println("ERROR: Tried to handle event of type ${event.type} by handler for type $handlerType!")
+            } else {
+                handler(event)
+            }
+        }
+    }
 
     val eventHandlers = mutableMapOf(
             *GameEventTypes.values().map {
@@ -18,12 +26,12 @@ object EventSystem {
     val handlersByKey = mutableMapOf<String, GameEventTypeHandler>()
 
     /**
-     * Add handler for specified event type,
+     * Add handler for specified event handlerType,
      * return unique handler UUID key by which its possible to remove it later
      */
     fun addHandler(eventType: GameEventTypes, handlerMethod: (GameEvent) -> Unit): String {
         return UUID.randomUUID().toString().apply {
-            val typeHandler = GameEventTypeHandler(type = eventType, handler = handlerMethod)
+            val typeHandler = GameEventTypeHandler(handlerType = eventType, handler = handlerMethod)
             (eventHandlers[eventType] ?: mutableListOf<GameEventTypeHandler>().apply {
                 eventHandlers[eventType] = this
             }).add(typeHandler)
@@ -34,7 +42,7 @@ object EventSystem {
     fun removeHandler(handlerKey: String) {
 
         handlersByKey[handlerKey]?.let { typeHandler ->
-            eventHandlers[typeHandler.type]?.remove(typeHandler)
+            eventHandlers[typeHandler.handlerType]?.remove(typeHandler)
         }
     }
 
@@ -47,11 +55,12 @@ object EventSystem {
 
         val handlersList = eventHandlers[event.type]
 
-        println("submitting event ${event.type} to ${handlersList?.size ?: 0} handlers...")
+        println("submitting event ${event.type} ${event.data?.let { "with DATA of type ${it.javaClass.name}" }} " +
+                "to ${handlersList?.size ?: 0} handlers...")
 
         handlersList?.let { handlers ->
             handlers.forEach { handler ->
-                handler.handler(event)
+                handler.handle(event)
             }
         }
     }
